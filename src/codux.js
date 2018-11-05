da305eaac3e10b0483479ef75f2013e460a7b954
ayleesedai <andrea.briozzo@gmail.com>
2018-11-05 10:59:51 +0100

    Prettyfied

--- src/codux.js
+++ src/codux.js
@@ -1,27 +1,35 @@
-import React, { createContext, useContext, useMemo, useReducer as useReactReducer } from 'react';
+import React, {
+    createContext,
+    useContext,
+    useMemo,
+    useReducer as useReactReducer
+} from "react";
 
 const context = createContext();
 
 const Provider = ({ middlewares, children }) => {
-    const wrapReducer = useMemo(() => reducer => {
-        const [state, dispatch] = reducer;
-        const wrappedDispatch = action => {
-            if (!middlewares || middlewares.length === 0) {
-                dispatch(action);
-            } else {
-                for(let i=0; i<middlewares.length; ++i)
-                    middlewares[i](dispatch, action, state);
-            }
-        }
-        return [state, wrappedDispatch];
-    }, [middlewares]);
-    return <context.Provider value={wrapReducer}>{children}</context.Provider>
+    const wrapReducer = useMemo(
+        () => ([state, dispatch]) => {
+            const wrappedDispatch = action => {
+                if (!middlewares || middlewares.length === 0) {
+                    dispatch(action);
+                } else {
+                    middlewares.forEach(middleware =>
+                        middleware(dispatch, action, state)
+                    );
+                }
+            };
+            return [state, wrappedDispatch];
+        },
+        [middlewares]
+    );
+    return <context.Provider value={wrapReducer}>{children}</context.Provider>;
 };
 
 const useReducer = (reducer, initialState) => {
     const reactReducer = useReactReducer(reducer, initialState);
     const wrapReducer = useContext(context);
     return wrapReducer(reactReducer);
-}
+};
 
 export { Provider, useReducer };
